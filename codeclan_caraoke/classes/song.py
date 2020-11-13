8f81f9df549e4f74f6f2b22c1bac8b1c525acb88
Iain Forbes <i.forbes77@gmail.com>
2020-11-13 01:07:57 +0000

    add guest

---
+++ codeclan_caraoke/classes/song.py
@@ -0,0 +1,6 @@
+class Song:
+
+    def __init__(self, name, genre):
+        song_catlouge = []
+        self.name = name
+        self.self = genre
--- codeclan_caraoke/tests/guest_test.py
+++ codeclan_caraoke/tests/guest_test.py
@@ -1,7 +1,17 @@
 import unittest
+from classes.room import Room
+from classes.song import Song
+from classes.guest import Guest
+
 
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
-       
+        self.guest_1 = Guest("Baron Humperdink", 17)
+        self.guest_2 = Guest("Sir Hamalot", 43)
         
+    def test_guest_name_and_age(self):
+       self.assertEqual("Baron Humperdink", self.guest_1.name)
+
+    def test_guest_age(self):
+        self.assertEqual(17, self.guest_1.age)
