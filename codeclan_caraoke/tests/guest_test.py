3a4dd5791fdb1028af5828180779f1b60a9f42c9
Iain Forbes <i.forbes77@gmail.com>
2020-11-15 10:52:13 +0000

    added comments

--- codeclan_caraoke/tests/guest_test.py
+++ codeclan_caraoke/tests/guest_test.py
@@ -6,15 +6,19 @@ from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
 
+    #Setup test guest 
     def setUp(self):
-        self.guest_1 = Guest("Baron Von Humperdink", 20, 40)
+        self.guest = Guest("Baron Von Humperdink", 20, 40)
         
+    #Test, test guest has name
     def test_guest_name(self):
-       self.assertEqual("Baron Von Humperdink", self.guest_1.name)
+       self.assertEqual("Baron Von Humperdink", self.guest.name)
 
+    #Test, test guest has age
     def test_guest_age(self):
-        self.assertEqual(20, self.guest_1.age)
+        self.assertEqual(20, self.guest.age)
 
+    #Test, test guest has wallet, with cash (40)
     def test_guest_wallet(self):
-        self.assertEqual(40, self.guest_1.wallet)
+        self.assertEqual(40, self.guest.wallet)
 
--- codeclan_caraoke/tests/room_test.py
+++ codeclan_caraoke/tests/room_test.py
@@ -10,17 +10,20 @@ class TestRoom(unittest.TestCase):
         self.guest = Guest("Baron Humperdink", 20, 40)
         self.song_1 =  Song("Saviour",  "Rise Against")
      
-
+    #Test room has name 
     def test_check_room_number(self):
         self.assertEqual("Karoke Room 1", self.room.room_number)
         
+    #Test room starts with no guests
     def test_room_starts_unbooked(self):
         self.assertEqual(0, self.room.booked_count())
 
+    #Test booking function adds guest to room 
     def test_room_booking(self):
         self.room.book_room(self.guest)
         self.assertEqual(1, self.room.booked_count())
         
+    #Test song is added to room 
     def test_add_song_to_room(self):
         self.assertEqual(self.room.song_selection, "Saviour")
 
